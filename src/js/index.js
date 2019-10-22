b7b261c2bd21879350e1c9417073455f40d0a12f
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-10-22 20:04:43 -0300

    Move to webpack

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '../scss/main.scss';
+import './autotrack';
+import '../_redirects';
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  },
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/',
+              publicPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/fonts/',
+              publicPath: 'assets/fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /_redirects/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false
+      }
+    }),
+    new MiniCssExtractPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: './src/img/logo_nolabel.png',
+      outputPath: '/assets/favicon',
+      favicons: {
+        appName: 'Romildo Corretor',
+        lang: 'pt-BR',
+        start_url: '/'
+      }
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
