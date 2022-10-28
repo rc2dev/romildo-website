b161c315c89f058e6841f7739f1f989ee1a2ecc7
Rafael Cavalcanti <dev@rafaelc.org>
2022-10-28 18:46:46 -0300

    Run ncu -u; update config; remove obsolete plugin

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   devServer: {
-    disableHostCheck: true,
+    port: 9000,
   },
   entry: './src/js/index.js',
   output: {
