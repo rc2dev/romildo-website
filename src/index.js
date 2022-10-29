b125a47f54484876f2931f0841dd00ed974889e0
Rafael Cavalcanti <dev@rafaelc.org>
2022-10-29 09:17:54 -0300

    Move fontawesome to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './js/home';
 import './js/modernizr.custom';
 import './scss/main.scss';
+
+// FontAwesome
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/regular.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
